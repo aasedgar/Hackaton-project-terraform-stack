c1bb015c303fb217a04287ebdb8259186849d07c
VINICIUS BRIOTTO <65972807+vbriotto@users.noreply.github.com>
2020-09-04 18:09:54 -0300

    Update state.tf

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-dvp2-335224"
+    bucket = "${format("hackathon-fiap-dvp2-335224-${terraform.workspace}")}"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
